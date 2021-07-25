ec743ea4073bd01a1365c34c330e4cf86348bca2
luismts <deivi_0326@hotmail.com>
2021-07-24 23:51:09 -0400

    Removed unnecesry directives

--- AppTrackingTransparency.iOS/AppTrackingTransparencyPermission.cs
+++ AppTrackingTransparency.iOS/AppTrackingTransparencyPermission.cs
@@ -1,10 +1,6 @@
-﻿using Foundation;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using UIKit;
 using Xamarin.Essentials;
 using XEssentialsTest.Extensions;
 using static Xamarin.Essentials.Permissions;
